5f5c707f77dbdefc6f5461e5814bd6738e1c2a06
BlazingInfernoDragon123 <96913755+BlaizingInfernoDragon123@users.noreply.github.com>
2022-10-19 00:03:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function setCookie(Default, value, options = {}) {
 
   options = {
-    path: 'https://blaizinginfernodragon123.github.io/copy-to-clipboard-button-in-html-and-css-and-javascript-/',
+    path: 'https://blaizinginfernodragon123.github.io/login-form-UI-using-only-html-css-and-javascript-/',
     // add other defaults here if necessary
     ...options
   };
